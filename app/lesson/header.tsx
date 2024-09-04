d9f7c7f3b765241d18ee867d6c9ad2bd52cb0a41
Hashaam-Ejaz <hashaamejaz@gmail.com>
2024-09-04 13:18:51 +0500

    Lesson Header completed

--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -1,3 +1,7 @@
+import { Progress } from "@/components/ui/progress";
+import { InfinityIcon, X } from "lucide-react";
+import Image from "next/image";
+
 type props = {
   hearts: number;
   percentage: number;
@@ -10,8 +14,26 @@ export const Header = ({
   hasActiveSubscription,
 }: props) => {
   return (
-    <div>
-      <h1>This is the header</h1>
-    </div>
+    <header className="mx-auto flex w-full max-w-[1140px] items-center justify-between gap-x-7 px-10 pt-[20px] lg:pt-[50px]">
+      <X
+        onClick={() => {}}
+        className="cursor-pointer text-slate-500 transition hover:opacity-75"
+      />
+      <Progress value={percentage} />
+      <div className="flex items-center font-bold text-rose-500">
+        <Image
+          src={"/heart.png"}
+          height={28}
+          width={28}
+          alt="Hearts"
+          className="mr-2"
+        />
+        {hasActiveSubscription ? (
+          <InfinityIcon className="h-6 w-6 stroke-[3]" />
+        ) : (
+          hearts
+        )}
+      </div>
+    </header>
   );
 };
